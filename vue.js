d21bcbb94ffeb1e24fd7bee9eacea3d2c01f06f0
hernando-code <hernaranjo@gmail.com>
2020-07-28 17:44:33 -0500

    crear una instancia de vue

--- vue.js
+++ vue.js
@@ -1,1 +1,15 @@
 console.log('working')
+
+const app = new Vue({//se debe crear una instancia de veu, asi se llama toda la libreria
+    el: '#app', 
+    data: {
+        titulo: 'hola mundo con vue',
+        frutas: ['peras', 'manzanas'],
+        fruit:[
+            {nombre:'pera', cantidad: 10 },
+            {nombre:'banano', cantidad: 15 },
+            {nombre:'manzana', cantidad: 0 }
+        ]
+    }
+
+})
